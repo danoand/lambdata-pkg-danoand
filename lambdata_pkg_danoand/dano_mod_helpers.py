a95bf28b4f10f670b15f76e86d98a8fefb13c190
Dan Anderson <danfanderson@gmail.com>
2020-03-12 00:47:54 -0500

    update functions

--- lambdata_pkg_danoand/dano_mod_helpers.py
+++ lambdata_pkg_danoand/dano_mod_helpers.py
@@ -34,7 +34,7 @@ def list2column(lst, df, new_col_name='new_col_name'):
 
     # Is there a column name of 'new_column' in the passed dataframe
     if new_col_name in df.columns:
-        ts = datetime.datetime.now().timestamp().isoformat()
+        ts = datetime.datetime.now().strftime("%m/%d/%Y_%H:%M:%S")
         new_col_name = new_col_name + "_" + ts
 
     df_tmp = pd.DataFrame({new_col_name: pd.Series(lst)})
@@ -56,38 +56,47 @@ def date2year_month_day(df, col_name):
 
     if len(col_name) == 0:
         # missing column name
-        print(f'passed "dataframe" is empty')
+        print(f'"column name" is missing')
         return
 
     if not col_name in df.columns:
         # column name not found in the passed dataframe
-        print(f'passed "dataframe" is empty')
+        print(f'column name "{col_name}" not found in dataframe')
         return
 
     # Create a series of data time objects
     tmp_dt_tm_srs = pd.to_datetime(df[col_name], infer_datetime_format=True)
 
     # Is there a column name of 'day' in the passed dataframe
+    ts = datetime.datetime.now().strftime("%m/%d/%Y_%H:%M:%S")
     col_name_day = 'day'
     if col_name_day in df.columns:
-        ts = datetime.datetime.now().timestamp().isoformat()
         col_name_day = col_name_day + "_" + ts
 
     # Is there a column name of 'month' in the passed dataframe
     col_name_month = 'month'
     if col_name_month in df.columns:
-        ts = datetime.datetime.now().timestamp().isoformat()
         col_name_month = col_name_month + "_" + ts
 
     # Is there a column name of 'year' in the passed dataframe
     col_name_year = 'year'
     if col_name_year in df.columns:
-        ts = datetime.datetime.now().timestamp().isoformat()
         col_name_year = col_name_year + "_" + ts
 
+    # Create dataframe comprised 
+    df_tmp = pd.DataFrame({
+        col_name_day: pd.to_datetime(tmp_dt_tm_srs, infer_datetime_format=True).apply(get_date_day),
+        col_name_month: pd.to_datetime(tmp_dt_tm_srs, infer_datetime_format=True).apply(get_date_month),
+        col_name_year: pd.to_datetime(tmp_dt_tm_srs, infer_datetime_format=True).apply(get_date_year)})
 
+    return pd.concat([df, df_tmp], axis=1)
 
-    
+def get_date_day(obj):
+    return obj.day
 
+def get_date_month(obj):
+    return obj.month
 
+def get_date_year(obj):
+    return obj.year
 
--- test_script.py
+++ test_script.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-from lambdata_pkg_danoand.dano_mod_helpers import list2column
+from lambdata_pkg_danoand.dano_mod_helpers import list2column, date2year_month_day
 
 df = pd.DataFrame({"state": ["CT", "CO", "CA", "TX"]})
 lst = [True, False, True, True]
@@ -8,3 +8,9 @@ lst = [True, False, True, True]
 df_new = list2column(lst, df, new_col_name='col_bool')
 
 print(df_new)
+
+df_dts = pd.DataFrame({"my_date": ['2010-01-01', '2010-01-02', '2010-01-03']})
+
+df_dts_new = date2year_month_day(df_dts, 'my_date')
+
+print(f'\n\n{df_dts_new}')
